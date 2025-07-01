f1008e1a2ebfa564b597eaf6486aa65dda5f1fb6
Eduardo dos Santos Sousa <eduardosousa.eds@gmail.com>
2025-06-30 23:38:12 -0300

    Commit

--- recipes/models.py
+++ recipes/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image  # <- IMPORTANTE!
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+    def __str__(self):
+        return self.name
+
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
@@ -13,10 +22,29 @@ class Recipe(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d')
-    
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d', blank=True, default='')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, default=None)
 
+    def __str__(self):
+        return self.title
 
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
 
+        # Redimensionar imagem se existir
+        if self.cover:
+            try:
+                img = Image.open(self.cover.path)
+
+                # Define tamanho máximo (ajuste conforme necessário)
+                max_width = 800
+                max_height = 600
+
+                if img.height > max_height or img.width > max_width:
+                    img.thumbnail((max_width, max_height))
+                    img.save(self.cover.path)
+            except Exception as e:
+                print(f"Erro ao redimensionar imagem: {e}")
 
     
--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'recipes'  
 
 urlpatterns = [
-    path('', views.home, name="recipes-home"), #Home page
-    path('recipes/<id>/', views.recipe, name="recipes-recipe"), #Home page
+    path('', views.home, name='home'),  # <- isso cria a rota 'recipes:home'
+    path('recipe/<int:id>/', views.recipe, name='recipe'),
+    path('category/<int:category_id>/', views.category, name='category'),
 ]
