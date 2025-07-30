c14061002aa8b0070208d0c9b945b45930c058a7
Eduardo dos Santos Sousa <eduardosousa.eds@gmail.com>
2025-07-30 01:01:40 -0300

    Commit

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
 from recipes import views
+from recipes.models import Category, Recipe, User
+
 
 
 class RecipeViewTest(TestCase):
@@ -24,7 +26,36 @@ class RecipeViewTest(TestCase):
             )    
 
     def test_recipe_home_template_loads_recipes(self):
-        ...    
+        category = Category.objects.create(name='Category')
+        author = User.objects.create_user(
+            first_name = 'user',
+            last_name = 'name',
+            username = 'username',
+            password = '123456',
+            email = 'username@email.com',
+        )
+        recipe = Recipe.objects.create(
+            category = category,
+            author = author,
+            title = 'Recipe Title',
+            description = 'Recipe Description',
+            slug = 'recipe-slug',
+            preparation_time = 10,
+            preparation_time_unit = 'Minutos',
+            servings = 5,
+            servings_unit = 'Porções',
+            preparation_steps = 'Recipe Preparation Steps',
+            preparation_steps_is_html = False,
+            is_published = True)
+        
+        response = self.client.get(reverse('recipes:home'))
+        content = response.content.decode('utf-8')
+        response_context_recipes = response.context['recipes']
+        self.assertIn('Recipe Title', content)
+        self.assertIn('10 Minutos', content)
+        self.assertIn('5 Porções', content)
+        self.assertEqual(len(response_context_recipes), 1)
+        pass   
 
     def test_recipe_category_view_returns_404_if_no_recipes_found(self):
         response = self.client.get(reverse('recipes:category', kwargs={'category_id':1}))
