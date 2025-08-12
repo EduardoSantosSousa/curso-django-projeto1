42f9570f3d5f70cad257fe2ec076a030db78c676
Eduardo dos Santos Sousa <eduardosousa.eds@gmail.com>
2025-08-12 01:06:27 -0300

    Commit

--- recipes/views.py
+++ recipes/views.py
@@ -4,13 +4,34 @@ from recipes.models import Recipe
 from django.http import Http404
 from django.db.models import Q
 import unicodedata
+from django.core.paginator import Paginator
+from utils.pagination import make_pagination_range
+
+
 
 # Create your views here.
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
-    #recipes = get_list_or_404(Recipe.objects.filter(is_published=True).order_by('-id'))
-    return render(request, 'recipes/pages/home.html', context={'recipes':recipes})
+    
+    try:
+        current_page = int(request.GET.get('page', 1))
+    except ValueError:
+        current_page = 1
+
+    paginator = Paginator(recipes, 9)
+    page_obj = paginator.get_page(current_page)
+
+    pagination_range = make_pagination_range(
+        paginator.page_range, 
+        4,
+        current_page 
+    )
+
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes':page_obj,
+        'pagination_range':pagination_range
+        })
 
 def category(request, category_id):
     recipes = get_list_or_404(Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id'))
