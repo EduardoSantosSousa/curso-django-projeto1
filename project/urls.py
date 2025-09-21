63ddd3e4a45080dc08fa2e7834a11a92d353f41a
Eduardo <eduardosousa@innomotics.com>
2025-09-21 12:42:14 -0300

    Commit

--- project/urls.py
+++ project/urls.py
@@ -15,3 +15,7 @@ urlpatterns = [
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+
+# Servir arquivos estáticos (apenas no DEBUG)
+#if settings.DEBUG:
+#    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
