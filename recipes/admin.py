13cbdcaf2237c8eedfb4080430a68f6c376b6f1e
Eduardo <eduardosousa@innomotics.com>
2025-09-24 01:26:28 -0300

    Commit

--- recipes/admin.py
+++ recipes/admin.py
@@ -14,7 +14,7 @@ class RecipeAdmin(admin.ModelAdmin):
     list_per_page = 10
     list_editable = 'is_published',
     ordering = '-id',
-    prepopulated_fields = {"slug":('title')}
+    prepopulated_fields = {"slug":('title',)}
 
 admin.site.register(Category, CategoryAdmin)
 
