ba1628d1479c118b91ff49ffbee37a070396e9bb
Eduardo <eduardosousa@innomotics.com>
2025-09-25 01:38:28 -0300

    Commit

--- authors/views.py
+++ authors/views.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from recipes.models import Recipe
+from authors.forms.recipe_form import AuthorRecipeForm
 
 
 def register_view(request):
@@ -90,12 +91,49 @@ def dashboard(request):
 
 @login_required(login_url='authors:login', redirect_field_name='next')
 def dashboard_recipe_edit(request, id):
-    recipe = Recipe.objects.filter(is_published=False, author = request.user, pk=id, )
+    recipe = Recipe.objects.filter(is_published=False, author = request.user, pk=id, ).first()
 
     if not recipe:
         raise Http404()
+    
+    form = AuthorRecipeForm(data = request.POST or None, files = request.FILES or None, instance = recipe)
 
-    return render (request, 'authors/pages/dashboard_recipe.html', context={})
+    if form.is_valid():
+        # Agora, o form é valido e eu posso tentar salvar
+        recipe = form.save(commit=False)
+
+        recipe.author = request.user
+        recipe.preparation_steps_is_html = False
+        recipe.is_published = False
+
+        recipe.save()
+
+        messages.success(request, 'Sua receita foi salva com sucesso!')
+
+        return redirect(reverse('authors:dashboard_recipe_edit', args=(id,)))
+
+
+    return render (request, 'authors/pages/dashboard_recipe.html', context={'form':form})
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard_recipe_new(request):
+    form = AuthorRecipeForm(data = request.POST or None, files = request.FILES or None)
+
+    if form.is_valid():
+        recipe = form.save(commit=False)
+
+        recipe.author = request.user
+        recipe.preparation_steps_is_html = False
+        recipe.is_published = False
+
+        recipe.save()
+
+        messages.success(request, 'Salvo com Sucesso!')
+
+        return redirect(reverse('authors:dashboard_recipe_edit', args=(recipe.id,)))
+
+
+    return render (request, 'authors/pages/dashboard_recipe.html', context={'form':form, 'form_action':reverse('authors:dashboard_recipe_new')})
 
 
 
