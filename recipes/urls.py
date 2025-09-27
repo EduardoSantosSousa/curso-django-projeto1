e9fcf1d0d17747d0655cd0ba6f865bcbde9095e5
Eduardo <eduardosousa@innomotics.com>
2025-09-27 17:54:11 -0300

    Commit

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'recipes'  
 
 urlpatterns = [
-    path('', views.RecipeListViewBase.as_view(), name='home'),  # <- isso cria a rota 'recipes:home'
-    path('recipes/search/', views.search, name='search'),
+    path('', views.RecipeListViewHome.as_view(), name='home'),  # <- isso cria a rota 'recipes:home'
+    path('recipes/search/', views.RecipeListViewSearch.as_view(), name='search'),
     path('recipe/<int:id>/', views.recipe, name='recipe'),
-    path('category/<int:category_id>/', views.category, name='category'),
+    path('category/<int:category_id>/', views.RecipeListViewCategory.as_view(), name='category'),
     
 ]
