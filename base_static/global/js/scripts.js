4843d30c0d8d27a85f3c9db09280004cc13ef5b1
Eduardo <eduardosousa@innomotics.com>
2025-10-10 08:55:06 -0300

    Commit

--- base_static/global/js/scripts.js
+++ base_static/global/js/scripts.js
@@ -46,6 +46,19 @@ function my_scope() {
 
     })();
 
+    (() =>{
+        const authorsLogoutLinks = document.querySelectorAll('.authors-logout-link');
+        const formLogout = document.querySelector('.form-logout');
+
+        for (const link of authorsLogoutLinks) {
+            link.addEventListener('click', (e) => {
+                e.preventDefault();
+                formLogout.submit();
+            });
+        }
+
+    })();
+
 
 
 }
